944950710b4be7da7411019c0dcbd2ba9582a303
alterrant <pandor246@mail.ru>
2021-09-19 15:11:33 +0300

    lesson 53 refactor: change func UsersPage to class

--- src/components/Users/UsersPage.jsx
+++ src/components/Users/UsersPage.jsx
@@ -1,21 +1,27 @@
+import React from 'react';
 import UserContainer from "./User/UserContainer";
-import UsersStyle from './UsersPage.module.css'
+import UsersStyle from './UsersPage.module.css';
 import * as axios from "axios";
 
-function UsersPage (props) {
-  if(props.users.length === 0)
-   axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(function (response) {
-        debugger
-        props.setUsers( response.data.items );
-      })
-      ;
+class UsersPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let usersArr = props.users.map( user => <UserContainer user={user} /> )
-  return (
-        <div className={UsersStyle.wrapper}>
-          {usersArr}
-        </div>
-  )
+    if (this.props.users.length === 0)
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+          .then(function (response) {
+            debugger
+            props.setUsers(response.data.items);
+          })
+  }
+
+  render() {
+    let usersArr = this.props.users.map(user => <UserContainer user={user}/>)
+
+    return <div className={UsersStyle.wrapper}>
+      {usersArr}
+    </div>
+  }
 }
+
 export default UsersPage;
